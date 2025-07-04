533ad6e08c541a859cf1bfb2ce99483eeed0bad0
maazu <maaz581@gmail.com>
2025-07-04 02:37:23 +0100

    refactor: removed unsused deps

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { buttonVariants } from '@/components/ui/button';
-
 const Footer: React.FC = () => {
   return (
     <footer>
@@ -9,17 +7,6 @@ const Footer: React.FC = () => {
         <p className='text-sm text-muted-foreground'>
           &copy; {new Date().getFullYear()} NeurogenesAi. All rights reserved.
         </p>
-        {/* <nav className='flex space-x-4 mt-4 sm:mt-0'>
-          <Link href='/about' className={buttonVariants({ variant: 'link' })}>
-            About
-          </Link>
-          <Link href='/contact' className={buttonVariants({ variant: 'link' })}>
-            Contact
-          </Link>
-          <Link href='/privacy' className={buttonVariants({ variant: 'link' })}>
-            Privacy Policy
-          </Link>
-        </nav> */}
       </div>
     </footer>
   );
